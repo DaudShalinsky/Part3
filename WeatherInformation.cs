e4047fe1ff1aef3653a6061376cefeb9e3d3c71c
= <=>
2023-11-06 20:24:08 +0300

    Черновик

--- WeatherInformation.cs
+++ WeatherInformation.cs
@@ -10,13 +10,15 @@ public static class WeatherInformation
         Random number = new Random();
         for (int i = 0; i < temperatures.Length; i++)
         {
-            temperatures[i] = number.Next(-40, 40);
+            temperatures[i] = number.Next(-15, 40);
+            Console.WriteLine($"-- {temperatures[i]}");
         }
 
         Random numbers = new Random();
         for (int i = 0; i < precipitation.Length; i++)
         {
             precipitation[i] = number.Next(0, 10);
+            Console.WriteLine($"__{precipitation[i]}");
         }
 
         int averageTemperature = 0;
@@ -29,30 +31,75 @@ public static class WeatherInformation
 
         averageTemperature /= temperatures.Length;
 
+        Console.WriteLine(averageTemperature);
+
 
         //Расчет общего количества осадков за месяц
         int precipitations = 0;
         do
         {
             for (int i = 0; i < precipitation.Length; i++)
-            { precipitations += temperatures[i]; }
+            { precipitations += precipitation[i]; }
         } while (precipitation.Length < 30);
+
+        Console.WriteLine(precipitations);
 
         //Среднее количество осадков за месяц
         precipitations /= precipitation.Length;
+
+        Console.WriteLine(precipitations);
 
 
         //Дни с количеством осадков выше среднего
         int[] precipitationIsAboveAverage = { };
 
-        for (int i = 0; i < precipitation.Length; i++)
+        /*for (int i = 0; i < precipitation.Length; i++)
         {
             if (precipitation[i] > precipitations)
             {
                 precipitationIsAboveAverage[i] = precipitation[i];
+                Console.WriteLine(precipitationIsAboveAverage.Length);
             }
-        }
+        }*/
 
+
+        /*foreach (int c in precipitation)
+        {
+
+            if (c > precipitations)
+            {
+                for (int i = 0; i < 30; i++)
+                {
+                    precipitationIsAboveAverage[i] = c;
+
+                }
+            }
+        }*/
+        /*int x = 0;
+        for (int i = 0; i < precipitation.Length; i++)
+        {
+            if (precipitation[i] > precipitations)
+            {
+                x++;
+                precipitationIsAboveAverage[x] = precipitation[i];
+            }
+        }*/
+
+        
+        Console.WriteLine(precipitationIsAboveAverage.Length);
+
+
+
+
+        /*int[] vseUsloviya = { };
+
+        for (int i = 0; i < temperatures.Length; i++)
+        {
+            if (temperatures[i] < 0)
+            {
+                if()
+            }
+        }*/
 
 
 
